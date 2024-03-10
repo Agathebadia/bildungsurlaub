c3db248ba6dfdfc8081fc19b8d866de5c7dea4d4
Agathe Badia <ab@honeypot.io>
2024-03-10 18:39:40 +0100

    feat(bildungsurlaub): Add request info and platform to look for bildungsurlaub

--- app/routes/home/controller.js
+++ app/routes/home/controller.js
@@ -8,4 +8,16 @@ export default class HomeController extends Controller {
     get onError() {
     return 'An error occurred, please try again';
   }
+
+  get copyContent() {
+    let text = document.getElementById('copyTextToClipboard').innerHTML;
+    const copy = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert('Content copied to clipboard');
+    } catch (err) {
+      alert.error('Failed to copy: ', err);
+    }
+  }
+  }
 }
