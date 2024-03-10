970c1fadf3edd1f1e53db01d1db1b07fc086407a
Agathe Badia <ab@honeypot.io>
2024-03-10 18:55:59 +0100

    fix(bildungsurlaub): Fix copy to clipboard

--- app/routes/home/controller.js
+++ app/routes/home/controller.js
@@ -1,23 +1,16 @@
 import Controller from '@ember/controller';
 
 export default class HomeController extends Controller {
-  get onSuccess() {
-    return 'Copied to clipboard';
-  }
-
-    get onError() {
-    return 'An error occurred, please try again';
-  }
-
-  get copyContent() {
-    let text = document.getElementById('copyTextToClipboard').innerHTML;
-    const copy = async () => {
+  // REMINDER: copy to clipboard only works on HTTPS
+  async copyContent(targetText) {
+    let text = document.getElementById(targetText).innerHTML;
     try {
-      await navigator.clipboard.writeText(text);
+      await
+      navigator.clipboard.writeText(text);
       alert('Content copied to clipboard');
     } catch (err) {
-      alert.error('Failed to copy: ', err);
+      alert('Failed to copy: ', err);
     }
-  }
+  // }
   }
 }
